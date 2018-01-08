f9d287a8d1b383c03889a7d935be3bf3e0456a38
Mahdi89 <jelodari.m@gmail.com>
2018-01-08 20:24:02 +0000

    include mem ftech/decode

--- main.go
+++ main.go
@@ -4,12 +4,21 @@
 
 package main
 
-import "fmt"
+import (
+	"bufio"
+	//	"bytes"
+	//	"encoding/binary"
+	"fmt"
+	"log"
+	"os"
+)
 
 type word uint32
 type LineAddress uint8
 type CRTAddress uint8
 type SSEMFunc uint8
+
+var Memory [100]word
 
 const (
 	JMP SSEMFunc = iota
@@ -28,36 +37,109 @@ type SSEMInst struct {
 	Func   SSEMFunc
 }
 
-func Fetch(MEMR <-chan SSEMInst, FD chan<- SSEMInst) {
+func Fetch(MEMR <-chan SSEMInst, FD chan<- string) {
 
-	//IR := <-MEMR
-	// TODO Fetch instruction from memory
-	// Increment PC
-	FD <- SSEMInst{
-		LineNo: 0,
-		CRTNo:  0,
-		Func:   STOP}
+	// Fetch data/inst. from file
+	// TODO read from MEMR chan.
+	file, err := os.Open("./data/gcd.raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		FD <- scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	file.Close()
 }
 
-func Decode(FD <-chan SSEMInst, DE chan<- SSEMInst) {
+func Decode(FD <-chan string, DE chan<- SSEMInst) {
 
-	IR := <-FD
-	DE <- IR
+	var s, i string
+	var h0, h1, h2, h3 byte
+
+	for {
+
+		IR := <-FD
+
+		fmt.Sscanf(IR, "%4s%4s", &s, &i)
+		fmt.Sscanf(i, "%1x%1x%1x%1x", &h0, &h1, &h2, &h3)
+
+		fmt.Println(h0, h1, h2, h3)
+
+		switch SSEMFunc(h0) {
+		case JMP:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   JMP}
+		case JRP:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   JRP}
+		case LDN:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   LDN}
+		case STO:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   STO}
+		case SUB:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   SUB}
+		case SUB_alt:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   SUB_alt}
+		case TEST:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   TEST}
+		case STOP:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   STOP}
+		default:
+			DE <- SSEMInst{
+				LineNo: 0,
+				CRTNo:  0,
+				Func:   STOP}
+		}
+
+	}
 }
 
 func Execute(DE <-chan SSEMInst, EW chan<- SSEMInst) {
 
-	IR := <-DE
-	// Execute the decoded instruction
-	MDR := IR
-	EW <- MDR
+	for {
+		IR := <-DE
+		// Execute the decoded instruction
+		MDR := IR
+		EW <- MDR
+	}
 }
 
 func WriteBack(EW <-chan SSEMInst, MEMW chan<- SSEMInst) {
 
-	MDR := <-EW
-	// TODO Writre back into memory
-	MEMW <- MDR
+	for {
+		MDR := <-EW
+		// TODO Writre back into memory
+		MEMW <- MDR
+	}
 }
 
 func main(
@@ -66,14 +148,17 @@ func main(
 ) {
 	// Inter stage channels
 	MEMR := make(chan SSEMInst, 1)
-	FD := make(chan SSEMInst, 1)
+	FD := make(chan string, 1)
 	DE := make(chan SSEMInst, 1)
 	EW := make(chan SSEMInst, 1)
 	MEMW := make(chan SSEMInst, 1)
 
-	// Processor stages
-	go Fetch(MEMR, FD)
-	go Decode(FD, DE)
-	go Execute(DE, EW)
-	go WriteBack(EW, MEMW)
+	for {
+		// Processor stages
+		go Fetch(MEMR, FD)
+		go Decode(FD, DE)
+		go Execute(DE, EW)
+		go WriteBack(EW, MEMW)
+	}
+
 }
